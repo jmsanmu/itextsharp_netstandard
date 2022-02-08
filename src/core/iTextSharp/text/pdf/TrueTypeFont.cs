f8b8d54f97edf820b9c6ab238d3fb545adaff41a
José Manuel Sánchez <jose-manuel.sanchez-munoz@capgemini.com>
2022-02-08 07:48:45 +0100

    Added autoregistration of machine encoding

--- src/core/iTextSharp/text/pdf/TrueTypeFont.cs
+++ src/core/iTextSharp/text/pdf/TrueTypeFont.cs
@@ -697,6 +697,7 @@ namespace iTextSharp.text.pdf {
         protected string ReadStandardString(int length) {
             byte[] buf = new byte[length];
             rf.ReadFully(buf);
+            System.Text.Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
             return System.Text.Encoding.GetEncoding(1252).GetString(buf);
         }
     
